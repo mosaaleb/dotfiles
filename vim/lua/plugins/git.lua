a5ef24b1e75491c1dc5a07fdfefedde652377b72
Muhammad <muhammed.ebeid@gmail.com>
2026-07-10 18:05:43 +0300

    use a pipe for the gitsigns delete sign

--- vim/lua/plugins/git.lua
+++ vim/lua/plugins/git.lua
@@ -17,7 +17,7 @@ require('gitsigns').setup({
   signs = {
     add          = { text = '│' },
     change       = { text = '│' },
-    delete       = { text = '_' },
+    delete       = { text = '|' },
     topdelete    = { text = '‾' },
     changedelete = { text = '│_' },
     untracked    = { text = '┆' },
